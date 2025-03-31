02fb4f90c30d1a614796fe21a899e23c7cd3cc80
Mayank Kumar Gautam <mayank.guts30@gmail.com>
2025-03-31 20:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        let divmaking = document.createElement("div");
-        divmaking.id = "dom";
-        document.body.appendChild(divmaking);
-
-        let domsuccess = document.getElementById("dom");
-        domsuccess.innerHTML = "DOM load success";
-    }, 5000); // Adding 5 seconds delay to ensure test completes
-});
+document.addEventListener("DOMContentLoaded", () => {
+            let divmaking = document.createElement("div");
+            divmaking.id = "dom";
+            divmaking.innerHTML = "DOM load success";
+            document.body.appendChild(divmaking);
+        });
